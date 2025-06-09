a2913f6080eade097940851389565030cc970338
ron-tcr2025 <ron@thecloudriver.com>
2025-06-08 21:41:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoicm9uanVkZC10Y3IiLCJhIjoiY21ibWIyYzZxMWJhazJscHZjc3VyNTNwNSJ9.HNj1Xnr4_GUk9b3Io9CNSg';
 
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9uanVkZC10Y3IiLCJhIjoiY21ibWIyYzZxMWJhazJscHZjc3VyNTNwNSJ9.HNj1Xnr4_GUk9b3Io9CNSg';
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-71.6856, 42.2067], // Grafton, MA
+  center: [-71.6856, 42.2067],
   zoom: 13
 });
 
-map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+let tracking = false, paused = false;
 
-// Optional: Center on user's live location
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(position => {
-    map.flyTo({
-      center: [position.coords.longitude, position.coords.latitude],
-      zoom: 15
-    });
-
-    new mapboxgl.Marker()
-      .setLngLat([position.coords.longitude, position.coords.latitude])
-      .addTo(map);
-  });
-}
 map.on('load', () => {
-  map.addSource('routes', {
-    type: 'geojson',
-    data: 'grafton_routes.geojson'
-  });
-
+  map.addSource('routes', { type: 'geojson', data: 'grafton_sample_routes.geojson' });
   map.addLayer({
     id: 'route-lines',
     type: 'line',
     source: 'routes',
-    paint: {
-      'line-color': '#ff0000',
-      'line-width': 2
-    }
+    paint: { 'line-color': '#ff0000', 'line-width': 4 }
   });
 });
+
+function startTracking() { tracking = true; paused = false; console.log('Tracking started'); }
+function pauseTracking() { paused = true; console.log('Tracking paused'); }
+function endTracking() { tracking = false; console.log('Tracking ended'); }
+function toggleStats() {
+  const stats = document.getElementById("stats");
+  stats.style.display = stats.style.display === "none" ? "block" : "none";
+}
+function downloadGPX() { alert("GPX export coming soon!"); }
