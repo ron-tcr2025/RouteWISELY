ef4ab1ca10bcef68bd57d8a08cc156b3bb4bcf96
Ron Judd - TheCloudRiver <ron@thecloudriver.com>
2025-06-21 15:44:03 -0400

    Update map_v6.dart
    Base map, Boston example, needs to add a polyline for segments, directions, and driver tracking

--- RouteWISELY_v6/map_v6.dart
+++ RouteWISELY_v6/map_v6.dart
@@ -1,1 +1,41 @@
 // map_v6.dart
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class RouteWiselyMap extends StatefulWidget {
+  @override
+  _RouteWiselyMapState createState() => _RouteWiselyMapState();
+}
+
+class _RouteWiselyMapState extends State<RouteWiselyMap> {
+  GoogleMapController? _controller;
+  static const LatLng _startLocation = LatLng(42.3601, -71.0589); // Boston
+
+  final Set<Marker> _markers = {
+    Marker(
+      markerId: MarkerId('start'),
+      position: _startLocation,
+      infoWindow: InfoWindow(title: 'Start Location'),
+    ),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('RouteWISELY Map')),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _startLocation,
+          zoom: 12,
+        ),
+        markers: _markers,
+        onMapCreated: (controller) {
+          _controller = controller;
+        },
+        myLocationEnabled: true,
+        compassEnabled: true,
+      ),
+    );
+  }
+}
