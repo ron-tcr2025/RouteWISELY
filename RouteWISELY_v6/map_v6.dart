05c9224e7f8391802e9f19446ee40d48a7fa0ce5
Ron Judd - TheCloudRiver <ron@thecloudriver.com>
2025-06-21 15:49:15 -0400

    Update map_v6.dart
    Added polylines, tracking, etc, started with Missoula, MT as example (v6) application. rj

--- RouteWISELY_v6/map_v6.dart
+++ RouteWISELY_v6/map_v6.dart
@@ -1,7 +1,8 @@
-// map_v6.dart
+// map_v6.dart - RouteWISELY Enhanced Map (Missoula, MT)
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 class RouteWiselyMap extends StatefulWidget {
   @override
@@ -10,31 +11,91 @@ class RouteWiselyMap extends StatefulWidget {
 
 class _RouteWiselyMapState extends State<RouteWiselyMap> {
   GoogleMapController? _controller;
-  static const LatLng _startLocation = LatLng(42.3601, -71.0589); // Boston
 
-  final Set<Marker> _markers = {
-    Marker(
-      markerId: MarkerId('start'),
-      position: _startLocation,
-      infoWindow: InfoWindow(title: 'Start Location'),
-    ),
-  };
+  // Missoula, MT as default center
+  static const LatLng _startLocation = LatLng(46.8721, -113.9940);
+
+  final Set<Marker> _markers = {};
+  final Set<Polyline> _polylines = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeMapElements();
+  }
+
+  void _initializeMapElements() {
+    // Add starting marker
+    _markers.add(
+      Marker(
+        markerId: MarkerId('start'),
+        position: _startLocation,
+        infoWindow: InfoWindow(title: 'Start Location'),
+      ),
+    );
+
+    // Example polyline (can be generated from API or Firestore)
+    _polylines.add(
+      Polyline(
+        polylineId: PolylineId('route1'),
+        points: [
+          _startLocation,
+          LatLng(46.8731, -113.9930),
+          LatLng(46.8741, -113.9920),
+        ],
+        color: Colors.blue,
+        width: 5,
+      ),
+    );
+  }
+
+  void _updateDriverPosition(LatLng position) {
+    setState(() {
+      _markers.removeWhere((m) => m.markerId.value == 'driver');
+      _markers.add(
+        Marker(
+          markerId: MarkerId('driver'),
+          position: position,
+          infoWindow: InfoWindow(title: 'Driver Position'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+        ),
+      );
+    });
+  }
+
+  Future<void> _syncWithFirestore() async {
+    // TODO: fetch route + driver data from Firestore
+  }
+
+  Future<void> _fetchDirections() async {
+    // TODO: implement Google Directions API call + polyline decoding
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('RouteWISELY Map')),
+      appBar: AppBar(
+        title: Text('RouteWISELY Map'),
+      ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: _startLocation,
-          zoom: 12,
+          zoom: 14,
         ),
-        markers: _markers,
         onMapCreated: (controller) {
           _controller = controller;
         },
+        markers: _markers,
+        polylines: _polylines,
         myLocationEnabled: true,
         compassEnabled: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_searching),
+        onPressed: () {
+          // Example dynamic update
+          _updateDriverPosition(LatLng(46.8735, -113.9935));
+        },
       ),
     );
   }
